da0a48ba3b49f1f3fd6d35ccb2ac08b558cd31dd
DSMarkoff <dsmarkov1@gmail.com>
2021-10-06 11:57:47 +0300

    Update Test.cs

--- Data/FoodSpot.Data.Models/Test.cs
+++ Data/FoodSpot.Data.Models/Test.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FoodSpot.Data.Models
+﻿namespace FoodSpot.Data.Models
 {
-    class Test
+    public class Test
     {
     }
 }
